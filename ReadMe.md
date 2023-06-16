ec54fbb00d204bb222e88bdbecc88244c3368e23
DCCoder90 <ernest.mallett@dccoders.com>
2023-06-16 14:41:48 -0500

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+## SDL Fun
+
+This is just a repo where I can store stuff as I'm learning [SDL](https://www.libsdl.org/) and trying to write a game from scratch in C++.  
+
+In order to compile this you will need [SDL](https://github.com/libsdl-org/SDL/releases/tag/release-2.26.5), ensure that the paths in the [CMakeLists.txt](CMakeLists.txt) file are pointing to where your files are, and also copy the dll to the directory with the final executable.  I plan on automating that last part at some point, just not right now.
+
+
+### Notebook
+
+If ```SDL_MAIN_HANDLED``` is not defined at the top of [main.cpp](main.cpp) then it will throw the following error:
+
+>undefined reference to `WinMain'
+
+**Fix**
+```c++
+#define SDL_MAIN_HANDLED
+```
---
+++ main.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("SDL count not initialize! SDL_ERROR: %s\n",SDL_GetError());
+    }else{
+        window = SDL_CreateWindow("Main Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640,480,SDL_WINDOW_SHOWN);
+        if(window == NULL){
+            printf("Window could not be created! SDL_ERROR: %s\n",SDL_GetError());
+        }else{
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,0xFF,0xFF,0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
+            SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,0xF0,0x00,0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(2000);
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
