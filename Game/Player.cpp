79a287ff8df0cd21e0ba04e933b92b3df70d4395
DCCoder90 <ernest.mallett@dccoders.com>
2023-07-02 17:17:04 -0500

    Added walking animation to player

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -1,14 +1,21 @@
 
 
 #include "Player.h"
+#include "../include/Components/SpriteSheet.h"
 
 Player::Player(GameObject &parent) : Component(parent) {
     name="Player";
 }
 
 void Player::Start() {
-    Sprite* sprite = new Sprite("../Assets/circle.png",parent);
-    parent.AddComponent(sprite);
+    //Sprite* sprite = new Sprite("../Assets/circle.png",parent);
+    //parent.AddComponent(sprite);
+    SpriteSheet* spriteSheet = new SpriteSheet("../Assets/walk-sheet.png",parent);
+    spriteSheet->RegisterAnimation("walkdown",0,0,96,96,8);
+    spriteSheet->SetCurrentAnimation("walkdown");
+    spriteSheet->Pause(false);
+
+    parent.AddComponent(spriteSheet);
     LOG_INFO << "Loaded Player";
 
     parent.box = {250,100,32,32};
