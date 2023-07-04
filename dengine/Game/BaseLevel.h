b53fc0e558b15fd5af9b76d65c88bfb31828c85b
DCCoder90 <ernest.mallett@dccoders.com>
2023-07-04 04:38:07 -0500

    Added serialization to BaseLevel

--- dengine/Game/BaseLevel.h
+++ dengine/Game/BaseLevel.h
@@ -2,12 +2,14 @@
 #define DENGINE_BASELEVEL_H
 
 #include <SDL.h>
+#include "../include/Utils/Serializer.h"
 #include "../include/System/GameLevel.h"
 #include "../include/System/GameStates.h"
 #include "../include/System/Game.h"
 #include "Player.h"
 #include "../include/System/Sound.h"
 #include "../include/System/AudioManager.h"
+
 
 using namespace dengine;
 /**
@@ -32,6 +34,7 @@ namespace DemoGame {
 
     private:
         class Player *player;
+        class Serializer<BaseLevel> *serializer;
     };
 }
 #endif //DENGINE_BASELEVEL_H
