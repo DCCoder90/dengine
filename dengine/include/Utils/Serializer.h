2f804c832ccd2dfbeff1483e4c70398399c3acbc
DCCoder90 <ernest.mallett@dccoders.com>
2023-07-04 04:15:50 -0500

    Implementation moved to header

--- dengine/include/Utils/Serializer.h
+++ dengine/include/Utils/Serializer.h
@@ -5,9 +5,10 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
-#include "../include/Utils/NanoLog.h"
+#include "NanoLog.h"
 
 namespace dengine {
+    template<typename T>
     class Serializer {
     public:
         /**
@@ -15,27 +16,24 @@ namespace dengine {
          * @param filePath The path to save to/load from
          */
         Serializer(std::string filePath);
-        template<typename T>
+
         /**
          * @brief Serialize and object and save it to a file
-         * @tparam T The type to serialize
          * @param obj The object to serialize
          * @return True if success
          */
         bool saveToFile(const T* obj);
-        template<typename T>
         /**
          * @brief Load data from a file and deserialize it back to an object
          * @tparam T The type to deserialize
          * @return The deserialized object
          */
         T loadFromFile();
-    private
+    private:
     /**
      * @brief File path to save to/load from
      */
         std::string path;
-        template <typename T>
         /**
          * @brief Serialize an object to a vector<char>
          * @tparam T The type of the object to serialize
@@ -55,7 +53,7 @@ namespace dengine {
          * @return A vector<char> containing serialized data
          */
         std::vector<char> loadSerializedData();
-        template <typename T>
+
         /**
          * @brief Deserialize data from a vector back into an object
          * @tparam T The type to deserialize
@@ -64,6 +62,82 @@ namespace dengine {
          */
         T deserializeData(const std::vector<char>& data);
     };
+
+
+    template<typename T>
+    Serializer<T>::Serializer(std::string filePath){
+        path = filePath;
+    }
+
+    template<typename T>
+    bool Serializer<T>::saveToFile(const T* obj){
+        std::vector<char> objData = serializeObject(obj);
+        return saveSerializedObject(objData);
+    }
+
+    template<typename T>
+    T Serializer<T>::loadFromFile(){
+        std::vector<char> objData = loadSerializedData();
+        return deserializeData(objData);
+    }
+
+    template <typename T>
+    std::vector<char> Serializer<T>::serializeObject(const T* obj) {
+        std::vector<char> buffer;
+        buffer.resize(sizeof(*obj));
+        std::memcpy(buffer.data(), obj, sizeof(*obj));
+        return buffer;
+    }
+
+    template<typename T>
+    bool Serializer<T>::saveSerializedObject(const std::vector<char>& serializedData) {
+        std::ofstream file(path, std::ios::binary);
+        if (!file) {
+            LOG_CRIT << "Failed to open file: " << path;
+            return false;
+        }
+
+        // Save serialized object to file
+        file.write(serializedData.data(), serializedData.size());
+        if (!file) {
+            LOG_CRIT << "Failed to write to file: " << path;
+            return false;
+        }
+
+        LOG_INFO << "Serialized object saved to file: " << path;
+        return true;
+    }
+
+    template <typename T>
+    T Serializer<T>::deserializeData(const std::vector<char>& data) {
+        T obj;
+        std::memcpy(&obj, data.data(), sizeof(obj));
+        return obj;
+    }
+
+    template <typename T>
+    std::vector<char> Serializer<T>::loadSerializedData() {
+        std::ifstream file(path, std::ios::binary | std::ios::ate);
+        if (!file) {
+            std::cerr << "Failed to open file: " << path << std::endl;
+            return {};
+        }
+
+        std::streamsize fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        std::vector<char> buffer(fileSize);
+        file.read(buffer.data(), fileSize);
+        if (!file) {
+            std::cerr << "Failed to read file: " << path << std::endl;
+            return {};
+        }
+
+        return buffer;
+    }
+
 }
+
+
 
 #endif //DENGINE_SERIALIZER_H
