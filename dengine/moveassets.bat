fff94b33aa62466672d8bc477d90b7e91d75cda6
DCCoder90 <ernest.mallett@dccoders.com>
2023-07-06 04:02:29 -0500

    Fixed CMake and moveassets.bat

--- dengine/moveassets.bat
+++ dengine/moveassets.bat
@@ -1,4 +1,7 @@
 @echo off
 @RD /S /Q "cmake-build-debug/Assets"
 robocopy Assets cmake-build-debug/Assets /E
-robocopy bin/ cmake-build-debug/ /E
+robocopy extern/bin/rmlui cmake-build-debug/ /E
+robocopy extern/bin/sdl cmake-build-debug/ /E
+robocopy extern/bin/sdl_img cmake-build-debug/ /E
+
